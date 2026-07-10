4d703568ab68c3848055c1014e1d35d4221bac1e
Black-EDG <yhurycristiamanampaquispe@gmail.com>
2026-07-10 12:59:37 -0500

    Actualizacion con varios ejercicios como mostrar mi nombre y otros aspectos

---
+++ hola_mundo/hola_mundo.py
@@ -0,0 +1,13 @@
+print("Hello, world")
+
+"""Este es un comentario de python
+en varias lineas"""
+
+''' Esto tambien 
+es
+un comentario en varias lineas de 
+python '''
+
+"Este tambien es un comentario de python"
+
+'Este tambien es para un comentario de python'
